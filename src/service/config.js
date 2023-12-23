281123ee97719c0fa0785d3645d451d8cbb75d92
Maxicro <maximo.crocce03@gmail.com>
2023-12-23 11:01:09 -0300

    arreglo nuevo

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 export const PORT = process.env.PORT || 8080
-=======
-export const PORT = 8080
->>>>>>> 2ae5fd1c171ee958e67835f813c533d424870b00
 export const MDB_CONNECT =  "mongodb+srv://AeroEnvios:C6j9Icq2vteLOykt@aeroenvios.favqeew.mongodb.net/adminAE"
 export const SESSION_SECRET = "AeroEnviosCategorys"
