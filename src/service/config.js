7f25686d69e8bc9013833fd6a2a5dbff9dfaa230
Maxicro <maximo.crocce03@gmail.com>
2023-12-23 10:53:06 -0300

    carga vercel

--- src/service/config.js
+++ src/service/config.js
@@ -1,3 +1,3 @@
-export const PORT = 8080
+export const PORT = process.env.PORT || 8080
 export const MDB_CONNECT =  "mongodb+srv://AeroEnvios:C6j9Icq2vteLOykt@aeroenvios.favqeew.mongodb.net/adminAE"
 export const SESSION_SECRET = "AeroEnviosCategorys"
